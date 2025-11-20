52fbb6b595d7cff95c5584230efecf8c9ff440d1
Marchenko Dmitriy <dimamarch2014@gmail.com>
2025-11-20 18:34:19 +0500

    Выполнил задание, все найденные ошибки выписал в README

---
+++ src/test/java/shopping/ShoppingServiceTest.java
@@ -0,0 +1,135 @@
+package shopping;
+
+import customer.Customer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import product.Product;
+import product.ProductDao;
+
+/**
+ * Тесты для сервиса покупок.
+ *
+ * @author Seraph-coder
+ * @since 14.11.2025
+ */
+@ExtendWith(MockitoExtension.class)
+public class ShoppingServiceTest {
+    private final ProductDao productDaoMock;
+    private final ShoppingService shoppingService;
+    private final Customer customer;
+
+    public ShoppingServiceTest(@Mock ProductDao productDao) {
+        this.productDaoMock = productDao;
+        this.shoppingService = new ShoppingServiceImpl(productDaoMock);
+        this.customer = new Customer(1L, "999-999-9999");
+    }
+
+    /**
+     * Проверка получения корзины для покупателя
+     * Ожидаемое поведение: всегда возвращается корзина, связанная с указанным покупателем
+     */
+    @Test
+    public void testGetCard() {
+
+        Cart cart = shoppingService.getCart(customer);
+
+        Assertions.assertNotNull(cart);
+    }
+
+    /**
+     * Проверка получения покупателем более чем одной корзины
+     * Ожидаемое поведение: при повторном вызове метода возвращается новая корзина
+     */
+    @Test
+    public void testGetMultipleCartsForCustomer() {
+        Cart cart1 = shoppingService.getCart(customer);
+        Cart cart2 = shoppingService.getCart(customer);
+
+        Assertions.assertNotSame(cart1, cart2);
+    }
+
+    /**
+     * Я считаю, что этот тест излишен, так как тестируемый метод просто делегирует вызов DAO слою.
+     * А в Unit тестах текущего класса мы его мокнули.
+     * Так что я оставил его пустым.
+     * Тестировать этот метод стоит в интеграционных тестах.
+     */
+    @Test
+    public void testGetAllProducts() {
+    }
+
+    /**
+     * Я считаю, что этот тест излишен, так как тестируемый метод просто делегирует вызов DAO слою.
+     * А в Unit тестах текущего класса мы его мокнули.
+     * Так что я оставил его пустым.
+     * Тестировать этот метод стоит в интеграционных тестах.
+     */
+    @Test
+    public void testGetProductByName() {
+    }
+
+    /**
+     * Проверка успешной покупки продукта
+     * Ожидаемое поведение: покупка совершается успешно, возвращается true, результат сохраняется в DAO,
+     * количество продукта уменьшается на купленное количество
+     */
+    @Test
+    public void testBuyProductSuccessfully() throws BuyException {
+        Cart cart = shoppingService.getCart(customer);
+
+
+        Product product = new Product( "Какой то продукт", 10);
+        cart.add(product, 2);
+        Assertions.assertTrue(shoppingService.buy(cart));
+        Mockito.verify(productDaoMock, Mockito.times(1)).save(product);
+        Assertions.assertEquals(8, product.getCount());
+    }
+
+    /**
+     * Проверка покупки с количеством продукта, равным доступному количеству
+     * Ожидаемое поведение: покупка совершается успешно, возвращается true, результат сохраняется в DAO,
+     * количество продукта станет нулевым
+     */
+    @Test
+    public void testBuyProductWithExactStock() throws BuyException {
+        Cart cart = shoppingService.getCart(customer);
+
+        Product product = new Product( "Какой то продукт", 5);
+        cart.add(product, 5);
+        Assertions.assertTrue(shoppingService.buy(cart));
+        Mockito.verify(productDaoMock, Mockito.times(1)).save(product);
+        Assertions.assertEquals(0, product.getCount());
+    }
+
+    /**
+     * Проверка покупки с недостаточным количеством продукта
+     * Ожидаемое поведение: выбрасывается исключение BuyException, покупка не совершается
+     */
+    @Test
+    public void testBuyProductWithInsufficientStock() {
+        Cart cart = shoppingService.getCart(customer);
+        Product product = new Product("Какой то продукт", 3);
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                cart.add(product, 5),
+                "Невозможно добавить товар 'Какой то продукт' в корзину," +
+                        "т.к. нет необходимого количества товаров"
+        );
+        Mockito.verify(productDaoMock, Mockito.never()).save(Mockito.any());
+    }
+
+    /**
+     * Проверка покупки с пустой корзиной
+     * Ожидаемое поведение: покупка не совершается, возвращается false
+     */
+    @Test
+    public void testBuyProductWithEmptyCart() throws BuyException {
+        Cart cart = shoppingService.getCart(customer);
+
+        Assertions.assertFalse(shoppingService.buy(cart));
+        Mockito.verify(productDaoMock, Mockito.never()).save(Mockito.any());
+    }
+}
